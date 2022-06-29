297a803a8ccd183d5889c683c8419ec9d819c80f
gian <gianfranco.jsx@gmail.com>
2022-06-29 16:49:59 -0300

    feat(StyledComponents) setted up for work locl in app

---
+++ src/PortfolioIntro/components/Background.js
@@ -0,0 +1,44 @@
+/* a background component with a tech image */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const BackgroundDiv = styled.div`
+    background-image: url(${props => props.image});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    min-height: 40vh;
+    position: relative;
+    @media (min-width: 768px) {
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        width: 100%;
+        height: 100%;
+        position: relative;
+    }
+`;
+
+const Background = ({ image, children }) => (
+    <BackgroundDiv image={image}>
+        {children}
+    </BackgroundDiv>
+);
+
+Background.defaultProps = {
+    image    : 'https://source.unsplash.com/random/1600x900',
+    children : null,
+};
+
+Background.propTypes = {
+    image    : PropTypes.string,
+    children : PropTypes.node,
+};
+export default Background;
--- src/PortfolioIntro/components/Intro.js
+++ src/PortfolioIntro/components/Intro.js
@@ -1,50 +1,32 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import {
-    AvatarContainer,
-    Title,
-    Subtitle,
-    IconsContainer,
-    GithubIcon,
-    LinkedinIcon,
-    MailIcon,
-    VerticalDivider,
-} from '../styles';
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Avatar from './Avatar';
+import styled from 'styled-components';
+import Background from './Background';
 
-const Avatar = ({ avatar, name, description }) => {
-    const [isHover, setIsHover] = useState(false);
-    const handleMouseEnter = () => setIsHover(true);
-    const handleMouseLeave = () => setIsHover(false);
-
-    return (
-        <AvatarContainer onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <img src={avatar} alt={name} />
-            <Title>{name}</Title>
-            <IconsContainer>
-                <GithubIcon />
-                <VerticalDivider />
-                <LinkedinIcon />
-                <VerticalDivider />
-                <MailIcon />
-            </IconsContainer>
-            {isHover && <Subtitle>{description}</Subtitle>}
-        </AvatarContainer>
-    );
-};
-Avatar.defaultProps = {
-    avatar      : 'https://avatars0.githubusercontent.com/u/17098281?s=460&v=4',
-    name        : 'John Doe',
-    description : 'I am a web developer',
-};
-Avatar.propTypes = {
-    avatar      : PropTypes.string,
-    name        : PropTypes.string,
-    description : PropTypes.string,
-};
-
+const HeroContainer = styled.div`
+    position: relative;
+    background-color: #fafafa;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 20px;
+    width: 100%;
+    height: 100%;
+    @media (min-width: 768px) {
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
+`;
 function Intro(props) {
     return (
-        <Avatar {...props} />
+        <HeroContainer>
+            <Background>
+                <Avatar {...props} />
+            </Background>
+        </HeroContainer>
     );
 }
 /*
