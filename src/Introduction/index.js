de48f10589f7fc2ba73cea4fa1c99caf8d62e390
rservera <renzoservera@gmail.com>
2022-08-12 15:04:06 -0300

    Introduction section

--- src/Introduction/index.js
+++ src/Introduction/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import {
-    HeadingHero,
-    HeadingMedium,
-    IconsContainer,
-    GithubIcon,
-    LinkedinIcon,
-    MailIcon,
-} from '../styles';
+import { HeadingHero, HeadingMedium } from '../styles';
 import useTypingText from '../helpers/useTypingText';
-
-const StyledHeadingMedium = styled(HeadingMedium)`
-    font-weight: normal;
-    margin-bottom: 20px;
-`;
+import { Github } from '@styled-icons/simple-icons/Github';
+import { LinkedinWithCircle } from '@styled-icons/entypo-social/LinkedinWithCircle';
+import { MailWithCircle } from '@styled-icons/entypo-social/MailWithCircle';
 
 const Introduction = ({ name, descriptions }) => {
     const { output } = useTypingText(
@@ -30,16 +21,23 @@ const Introduction = ({ name, descriptions }) => {
                 {`I'm ${name},`}
                 <br />
                 {'a '}
-                {output}
+                <IntroDescription>{output}</IntroDescription>
             </HeadingHero>
-            <IconsContainer>
-                <GithubIcon />
-                <LinkedinIcon />
-                <MailIcon />
-            </IconsContainer>
+            <IntroductionLinksWrapper>
+                <IntroductionLink href="https://github.com/GianfrancoCorrea" target="_blank">
+                    <Github />
+                </IntroductionLink>
+                <IntroductionLink href="https://www.linkedin.com/in/gianfranco-correa/" target="_blank">
+                    <LinkedinWithCircle />
+                </IntroductionLink>
+                <IntroductionLink href="mailto:" target="_blank">
+                    <MailWithCircle />
+                </IntroductionLink>
+            </IntroductionLinksWrapper>
         </>
     );
 };
+
 Introduction.defaultProps = {
     name: 'John Doe',
 };
@@ -48,4 +46,49 @@ Introduction.propTypes = {
     descriptions : PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
+const StyledHeadingMedium = styled(HeadingMedium)`
+    font-weight: normal;
+    margin-bottom: 20px;
+`;
+
+const IntroDescription = styled.span`
+    font-family: 'Space Mono', monospace;
+    letter-spacing: -7px;
+    word-spacing: -25px;
+    font-weight: 700;
+    font-style: italic;
+    font-size: 110%;
+`;
+
+const IntroductionLinksWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 30px;
+    margin-top: 40px;
+`;
+
+const IntroductionLink = styled.a`
+    color: currentColor;
+    text-decoration: none;
+    position: relative;
+    z-index: 1;
+
+    &:hover {
+        color: #38FF58;
+        cursor: pointer;
+
+        svg {
+            transform: scale(1.1);
+        }
+
+    }
+
+    svg {
+        height: 36px;
+        transition: all .2s ease-in-out;
+    }
+`;
+
 export default Introduction;
+
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 import * as variables from './variables';
-import { Github } from '@styled-icons/feather/Github';
-import { Linkedin } from '@styled-icons/feather/Linkedin';
-import { Mail } from '@styled-icons/feather/Mail';
 
 // Heading
 
@@ -132,38 +129,6 @@ const Table = styled.table`
     }
 `;
 
-const IconsContainer = styled.div`
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 40px;
-    margin-top: 60px;
-    svg {
-        width: 30px;
-        transition: all 0.1s ease-in-out;
-        cursor: pointer;
-        color: currentColor;
-        &:hover {
-            transform: scale(1.1);
-        }
-    }
-    `;
-const GithubIcon = styled(Github)`
-&:hover {
-color: #38FF58;
-}
-`;
-const LinkedinIcon = styled(Linkedin)`
-&:hover {
-color: #38FF58;
-}
-`;
-const MailIcon = styled(Mail)`
-&:hover {
-color: #38FF58;
-}
-`;
-
 export {
     Heading, HeadingMedium, HeadingLarge, HeadingXLarge, HeadingHuge, HeadingHero,
     Text,
@@ -171,5 +136,4 @@ export {
     Input, TextArea,
     Card,
     Table, TableHead, TableCell,
-    IconsContainer, GithubIcon, LinkedinIcon, MailIcon,
 };
